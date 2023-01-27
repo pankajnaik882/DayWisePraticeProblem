e6b2a5d38dcfaaa57a88d8c5661a628bdf82e2ee
“pankajnaik882” <pankaj080519999@gmail.com>
2023-01-27 20:32:38 +0530

    Add: Day6 Practice Problem Updated

--- day6/tableOfPowerTwo.sh
+++ day6/tableOfPowerTwo.sh
@@ -1,7 +1,7 @@
 read -p "Enter the number to print table : " n
-number = 2
-for ((i=1;i<=$n;i++))
+b=2
+for ((i=1;i<=n;i++))
 do
-	 table=$(($number**$i))
-	 echo "Table of $number**$i is : $table"
+table="$(($b ** $i))"
+echo "Table of $b*$i is : $table"
 done
